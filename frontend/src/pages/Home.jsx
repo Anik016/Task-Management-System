5ccea4751caccd70128ba3b51e41c49ab8daa784
Anik016 <127922585+Anik016@users.noreply.github.com>
2025-03-30 15:13:41 +0600

    home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,1 +1,13 @@
 
+import TrelloBoard from "../components/TaskBoard ";
+import Footer from "../components/Footer";
+
+const Home = () => {
+  return (
+    <div>
+      <TrelloBoard />
+      <Footer />
+    </div>
+  );
+};
+export default Home;
